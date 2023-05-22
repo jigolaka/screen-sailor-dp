224e9420bef3629680f5070fe6b4e1be0536a579
jigolaka <jigolaka@gmail.com>
2023-05-22 05:08:53 +0300

    Update camera class

--- sailor/process/camera.py
+++ sailor/process/camera.py
@@ -1,11 +1,12 @@
 import cv2
 import base64
+import numpy as np
 
 
 class Camera:
-    def __init__(self, cap_index=0):
+    def __init__(self, cap_index: int = 0, cap_flip: bool = False):
         self.cap = cv2.VideoCapture(cap_index)
-        self.cap_flip = False
+        self.cap_flip = cap_flip
 
         self.get_default_cap_settings()
 
@@ -19,27 +20,26 @@ class Camera:
         self.cap_saturation = self.cap.get(cv2.CAP_PROP_SATURATION)
         self.cap_sharpness = self.cap.get(cv2.CAP_PROP_SHARPNESS)
         self.cap_gamma = self.cap.get(cv2.CAP_PROP_GAMMA)
+        self.cap_flip = False
 
-    def process_frame(self):
-        if self.cap_flip:
-            _, self.frame = self.cap.read()
-            self.frame = cv2.flip(self.frame, 1)
+        return self.cap_brightness, self.cap_contrast, self.cap_hue, \
+            self.cap_saturation, self.cap_sharpness, self.cap_gamma, \
+            self.cap_flip
 
-        else:
-            _, self.frame = self.cap.read()
-
-        return self.frame
-
-    def convert_frame_base64(self, frame):
-        _, frame = cv2.imencode(".jpg", frame)
-        frame = base64.b64encode(frame)
-        frame = "data:image/jpg;base64," + frame.decode("ascii")
-
-        return frame
+    def set_default_cap_settings(self):
+        self.cap.set(cv2.CAP_PROP_BRIGHTNESS, self.cap_brightness)
+        self.cap.set(cv2.CAP_PROP_CONTRAST, self.cap_contrast)
+        self.cap.set(cv2.CAP_PROP_HUE, self.cap_hue)
+        self.cap.set(cv2.CAP_PROP_SATURATION, self.cap_saturation)
+        self.cap.set(cv2.CAP_PROP_SHARPNESS, self.cap_sharpness)
+        self.cap.set(cv2.CAP_PROP_GAMMA, self.cap_gamma)
 
     def set_cap_index(self, cap_index):
         self.set_default_cap_settings()
         self.cap = cv2.VideoCapture(cap_index)
+
+    def set_cap_flip(self, flip_value):
+        self.cap_flip = flip_value
 
     def set_cap_brightness(self, brightness_value):
         self.cap.set(cv2.CAP_PROP_BRIGHTNESS, brightness_value)
@@ -59,10 +59,19 @@ class Camera:
     def set_cap_gamma(self, gamma_value):
         self.cap.set(cv2.CAP_PROP_GAMMA, gamma_value)
 
-    def set_default_cap_settings(self):
-        self.cap.set(cv2.CAP_PROP_BRIGHTNESS, self.cap_brightness)
-        self.cap.set(cv2.CAP_PROP_CONTRAST, self.cap_contrast)
-        self.cap.set(cv2.CAP_PROP_HUE, self.cap_hue)
-        self.cap.set(cv2.CAP_PROP_SATURATION, self.cap_saturation)
-        self.cap.set(cv2.CAP_PROP_SHARPNESS, self.cap_sharpness)
-        self.cap.set(cv2.CAP_PROP_GAMMA, self.cap_gamma)
+    def process(self):
+        if self.cap_flip:
+            _, frame = self.cap.read()
+            frame = cv2.flip(frame, 1)
+
+        else:
+            _, frame = self.cap.read()
+
+        return frame
+
+    def as_base64(self, frame):
+        _, frame = cv2.imencode(".jpg", frame)
+        frame = base64.b64encode(frame)
+        frame = "data:image/jpg;base64," + frame.decode("ascii")
+
+        return frame
